f722bd51777b21f61efaf7ef49fdfdd119c0fcdc
rylieduval <129247479+rylieduval@users.noreply.github.com>
2023-11-08 19:02:50 -0600

    Update driver.java

--- driver.java
+++ driver.java
@@ -6,26 +6,26 @@ public class driver {
 		List<String> disciples = new theSinglyLinkedList<>();
 		//storing the 12 disciples names
 		disciples.append("Peter"); 
-        disciples.append("James");
-        disciples.append("John");
-        disciples.append("Andrew");
-        disciples.append("Philip");
-        disciples.append("Bartholomew");
-        disciples.append("Matthew");
-        disciples.append("Thomas");
-        disciples.append("James the Less");
-        disciples.append("Simon");
-        disciples.append("Thaddeus");
-        disciples.append("Judas Iscariot");
-        
-        //remove the one that betrayed Jesus
-        disciples.remove("Judas Iscariot");
-        
-        //sorting the names in alphabetical order
-        disciples.sort();
-        
-        // print the disciples in reverse order 
-        System.out.println("The Disciples are:");
-        disciples.printReverse();
+	        disciples.append("James");
+	        disciples.append("John");
+	        disciples.append("Andrew");
+	        disciples.append("Philip");
+	        disciples.append("Bartholomew");
+	        disciples.append("Matthew");
+	        disciples.append("Thomas");
+	        disciples.append("James the Less");
+	        disciples.append("Simon");
+	        disciples.append("Thaddeus");
+	        disciples.append("Judas Iscariot");
+	        
+	        //remove the one that betrayed Jesus
+	        disciples.remove("Judas Iscariot");
+	        
+	        //sorting the names in alphabetical order
+	        disciples.sort();
+	        
+	        // print the disciples in reverse order 
+	        System.out.println("The Disciples are:");
+	        disciples.printReverse();
 	}
 }
